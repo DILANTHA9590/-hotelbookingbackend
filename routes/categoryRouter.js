111c059133763d89b0ec3a42599c51c8449b7334
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-02-28 15:13:20 +0530

    create get category route and create get category by name name route🔗✅

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -2,11 +2,15 @@ import express from "express";
 import {
   createCategory,
   deleteCategory,
+  getCategoryItems,
+  getCategoryName,
 } from "../controllers/categoryController.js";
 
 const categoryRouter = express.Router();
 
 categoryRouter.post("/", createCategory);
+categoryRouter.get("/", getCategoryItems);
+categoryRouter.get("/:name", getCategoryName);
 
 categoryRouter.delete("/:name", deleteCategory);
 
