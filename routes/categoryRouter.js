27d9c402447ba89b95d12d6be826da87b9e71b99
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-02-28 19:24:29 +0530

    create update category route✅

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -4,12 +4,15 @@ import {
   deleteCategory,
   getCategoryItems,
   getCategoryName,
+  updateCategory,
 } from "../controllers/categoryController.js";
 
 const categoryRouter = express.Router();
 
 categoryRouter.post("/", createCategory);
+
 categoryRouter.get("/", getCategoryItems);
+categoryRouter.put("/:name", updateCategory);
 categoryRouter.get("/:name", getCategoryName);
 
 categoryRouter.delete("/:name", deleteCategory);
