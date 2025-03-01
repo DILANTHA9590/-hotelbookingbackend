95b383221a005de4df0ebe7fbdb173dcfed0eefd
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-03-01 18:21:19 +0530

    create room gerroom function🔗

--- routes/roomsRouter.js
+++ routes/roomsRouter.js
@@ -1,12 +1,14 @@
 import express from "express";
 import {
   createRoom,
+  getRooms,
   updateRoomDetails,
 } from "../controllers/roomsController.js";
 
 const roomRouter = express.Router();
 
 roomRouter.post("/", createRoom);
+roomRouter.get("/", getRooms);
 roomRouter.put("/:roomId", updateRoomDetails);
 
 export default roomRouter;
