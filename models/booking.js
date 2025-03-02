d72fbb3163080ce6a33b98ec824bed766a64f887
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-03-02 14:30:57 +0530

    create mokking model✅

---
+++ models/booking.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema({
+  bookingId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  roomId: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    default: "pending",
+  },
+  reason: {
+    type: String,
+    default: "",
+  },
+  start: {
+    type: Date,
+    required: true,
+  },
+  end: {
+    type: Date,
+    required: true,
+  },
+
+  notes: {
+    type: String,
+    default: "",
+  },
+
+  timeStamps: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Booking = mongoose.model("Bookings", bookingSchema);
+
+export default Booking;
