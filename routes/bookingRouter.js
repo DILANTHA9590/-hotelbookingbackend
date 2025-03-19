07d57c489d0f5e3f8175e37fa4ed1c3f4369b76e
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-03-19 23:18:06 +0530

    add route for update booking status✅

--- routes/bookingRouter.js
+++ routes/bookingRouter.js
@@ -4,6 +4,7 @@ import {
   deleteBooking,
   getAllBookings,
   updateBookingDetails,
+  updateBookingStatus,
 } from "../controllers/bookingController.js";
 
 const bookingrouter = express.Router();
@@ -11,6 +12,7 @@ const bookingrouter = express.Router();
 bookingrouter.post("/", createBooking);
 bookingrouter.get("/", getAllBookings);
 bookingrouter.delete("/:bookingId", deleteBooking);
+bookingrouter.put("/status/:bookingId", updateBookingStatus);
 bookingrouter.put("/:bookingId", updateBookingDetails);
 
 export default bookingrouter;
