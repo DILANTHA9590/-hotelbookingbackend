f07b0c15582ef22ac4d4865244fb7baa6d0ac7c5
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-03-19 23:17:37 +0530

    ✅ Add routes for updating and deleting gallery items

--- routes/galleryItemRoute.js
+++ routes/galleryItemRoute.js
@@ -1,12 +1,16 @@
 import express from "express";
 import {
+  deleteGalleryItem,
   getImageGallery,
   postGalleryItems,
+  updateGalley,
 } from "../controllers/galleryItemController.js";
 
 const galleryItemRouter = express.Router();
 
 galleryItemRouter.post("/", postGalleryItems);
 galleryItemRouter.get("/", getImageGallery);
+galleryItemRouter.delete("/:id", deleteGalleryItem);
+galleryItemRouter.put("/:id", updateGalley);
 
 export default galleryItemRouter;
