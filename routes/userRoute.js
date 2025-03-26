31b85e50a1a112153d212fcd9c1e056270cfe217
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-03-26 07:23:39 +0530

    add route for delete user 🌟✅ remove email verify testing route🗑️

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,20 +1,23 @@
 import express from "express";
 import {
+  deleteUser,
   getOneUser,
   getUser,
   loginUser,
   postUser,
-  sendSampleEmail,
+  sendOtpEmail,
   updateUserIsBlock,
+  verifyUserEmail,
 } from "../controllers/userControllers.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/", postUser);
+userRouter.delete("/:email", deleteUser);
 userRouter.get("/", getUser);
 userRouter.post("/login", loginUser);
 userRouter.post("/getUser", getOneUser);
 userRouter.put("/:email", updateUserIsBlock);
-userRouter.post("/email", sendSampleEmail);
+userRouter.post("/verifyemail", verifyUserEmail);
 
 export default userRouter;
