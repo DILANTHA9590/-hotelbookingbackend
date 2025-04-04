1d5177ed15f4cf849090bd91aff58f56512db5e9
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-04-04 19:07:10 +0530

    create route  for get all  rooms and get rooms by  room id🌟🔗✅

--- routes/roomsRouter.js
+++ routes/roomsRouter.js
@@ -2,15 +2,19 @@ import express from "express";
 import {
   createRoom,
   deleteRoom,
+  getRoombyId,
   getRooms,
+  getRoomsCustomers,
   updateRoomDetails,
 } from "../controllers/roomsController.js";
 
 const roomRouter = express.Router();
 
+roomRouter.get("/category", getRoomsCustomers);
 roomRouter.post("/", createRoom);
 roomRouter.post("/getall", getRooms);
 roomRouter.put("/:roomId", updateRoomDetails);
 roomRouter.delete("/:roomId", deleteRoom);
+roomRouter.get("/:roomId", getRoombyId);
 
 export default roomRouter;
